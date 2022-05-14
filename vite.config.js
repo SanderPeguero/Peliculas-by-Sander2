18c517f633996380a0c814cc934185e2b590f5d2
Sander <sanderpeguero.21@gmail.com>
2022-05-14 03:25:59 -0400

    .

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,20 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-            global: 'globalThis'
-        },
-        // Enable esbuild polyfill plugins
-        plugins: [
-            NodeGlobalsPolyfillPlugin({
-                buffer: true
-            })
-        ]
-    }
-  }
+  plugins: [react()]
+  // ,
+  // optimizeDeps: {
+  //   esbuildOptions: {
+  //       // Node.js global to browser globalThis
+  //       define: {
+  //           global: 'globalThis'
+  //       },
+  //       // Enable esbuild polyfill plugins
+  //       plugins: [
+  //           NodeGlobalsPolyfillPlugin({
+  //               buffer: true
+  //           })
+  //       ]
+  //   }
+  // }
 })
